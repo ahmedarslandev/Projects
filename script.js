0d2272f136a0c8378d30a82130668c6d94cd45a8
ahmedarslandev <148966385+ahmedarslandev@users.noreply.github.com>
2023-10-25 16:54:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+Shery.imageEffect("#effect", {
+  style: 5,
+
+  config: {
+    a: { value: 0.74, range: [0, 30] },
+    b: { value: -0.47, range: [-1, 1] },
+    zindex: { value: "0", range: [-9999999, 9999999] },
+    aspect: { value: 0.7964650145772595 },
+    gooey: { value: true },
+    infiniteGooey: { value: false },
+    growSize: { value: 4, range: [1, 15] },
+    durationOut: { value: 1, range: [0.1, 5] },
+    durationIn: { value: 1.5, range: [0.1, 5] },
+    displaceAmount: { value: 0.5 },
+    masker: { value: true },
+    maskVal: { value: 1.3, range: [1, 5] },
+    scrollType: { value: 0 },
+    geoVertex: { range: [1, 64], value: 1 },
+    noEffectGooey: { value: false },
+    onMouse: { value: 0 },
+    noise_speed: { value: 0.2, range: [0, 10] },
+    metaball: { value: 0.2, range: [0, 2] },
+    discard_threshold: { value: 0.5, range: [0, 1] },
+    antialias_threshold: { value: 0, range: [0, 0.1] },
+    noise_height: { value: 0.5, range: [0, 2] },
+    noise_scale: { value: 10, range: [0, 100] },
+  },
+});
